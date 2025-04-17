0b5194102c7c4e2dadef01b81e6ce30930a9205b
minz1988 <aminewh@hotmail.com>
2025-04-17 18:54:40 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function RivieraLandingPage() {
           <img
             src="/images/riviera-harbor.png"
             alt="Riviera Harbor"
-            className="rounded-2xl shadow-xl object-cover max-h-[400px] w-full"
+            className="rounded-2xl shadow-xl object-cover max-h-[350px] w-full"
           />
         </div>
       </section>
