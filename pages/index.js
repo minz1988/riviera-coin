c0f1b5a6f2d57f11b4328409f0bcbd859f4fd03a
minz1988 <aminewh@hotmail.com>
2025-04-17 17:54:55 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function RivieraLandingPage() {
         className="relative min-h-[85vh] flex flex-col items-center justify-center text-white text-center bg-cover bg-center"
         style={{ backgroundImage: "url('/images/RIV.png')" }}
       >
-        <div className="absolute top-10 w-24 h-24 bg-white rounded-full flex items-center justify-center shadow-lg">
+        <div className="absolute top-10 w-36 h-36 bg-white rounded-full flex items-center justify-center shadow-lg">
           <img src="/images/logo.png" alt="Riviera Coin Logo" className="w-16 h-16"/>
         </div>
         <div className="relative z-10 bg-black/50 p-8 rounded-xl max-w-2xl mt-32">
